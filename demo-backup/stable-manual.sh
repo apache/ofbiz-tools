de65d7e2685d295065a7e5212f654769bbb09822
Jacques Le Roux <jleroux@apache.org>
2017-06-25 07:30:16 +0000

    No functional change
    "--portoffset 10000" is unneeded when loading data in stable. It's only a local
    action.
    
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/tools@1799788 13f79535-47bb-0310-9956-ffa450edef68

--- demo-backup/stable-manual.sh
+++ demo-backup/stable-manual.sh
@@ -4,6 +4,6 @@ cd /home/ofbizDemo/branch16.11
 svn up
 ./gradlew "ofbiz --shutdown --portoffset 10000"
 ./gradlew cleanAll
-./gradlew "ofbiz --load-data --portoffset 10000"
+./gradlew "ofbiz --load-data"
 ./gradlew svnInfoFooter
 ./gradlew "ofbizBackground --start --portoffset 10000"
