95f63e7a9676f7d7b11c2a46eafa872d3b44cd8d
Jacques Le Roux <jleroux@apache.org>
2017-07-09 10:49:12 +0000

    No functional change, just make things more consistent with trunk-manual.sh
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/tools@1801367 13f79535-47bb-0310-9956-ffa450edef68

--- demo-backup/stable-manual.sh
+++ demo-backup/stable-manual.sh
@@ -4,6 +4,6 @@ cd /home/ofbizDemo/branch16.11
 svn up
 ./gradlew --no-daemon "ofbiz --shutdown --portoffset 10000"
 ./gradlew --no-daemon cleanAll
-./gradlew --no-daemon "ofbiz --load-data"
+./gradlew --no-daemon loadAll
 ./gradlew --no-daemon svnInfoFooter
 ./gradlew --no-daemon "ofbizBackground --start --portoffset 10000"
