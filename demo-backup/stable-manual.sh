02b74022befee0ef12d1471dd2d57c2cebace268
Jacques Le Roux <jleroux@apache.org>
2018-03-17 20:31:25 +0000

    Improved: gradlew --no-daemon "ofbiz --shutdown"  does not work (OFBIZ-10287)
    This is in relation with OFBIZ-10287. Better to not use these scripts for now.
    
    
    
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/tools@1827104 13f79535-47bb-0310-9956-ffa450edef68

--- demo-backup/stable-manual.sh
+++ demo-backup/stable-manual.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-cd /home/ofbizDemo/branch16.11
-svn up
-./gradlew --no-daemon "ofbiz --shutdown --portoffset 10000"
-./gradlew --no-daemon cleanAll
-./gradlew --no-daemon "ofbiz --load-data"
-./gradlew --no-daemon svnInfoFooter
-./gradlew --no-daemon "ofbizBackground --start --portoffset 10000"
+echo This does not work, see OFBIZ-10287. You need for now to use all-manual-nicely.sh
+
+#~ cd /home/ofbizDemo/branch16.11
+#~ svn up
+#~ ./gradlew --no-daemon "ofbiz --shutdown --portoffset 10000"
+#~ ./gradlew --no-daemon cleanAll
+#~ ./gradlew --no-daemon "ofbiz --load-data"
+#~ ./gradlew --no-daemon svnInfoFooter
+#~ ./gradlew --no-daemon "ofbizBackground --start --portoffset 10000"
--- demo-backup/trunk-manual.sh
+++ demo-backup/trunk-manual.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#~ #!/bin/sh
 
-cd /home/ofbizDemo/trunk
-svn up
-./gradlew --no-daemon pullAllPluginsSource
-./gradlew --no-daemon "ofbiz --shutdown"
-./gradlew --no-daemon cleanAll
-./gradlew --no-daemon loadAll
-./gradlew --no-daemon svnInfoFooter
-./gradlew --no-daemon ofbizBackground
+echo This does not work, see OFBIZ-10287. You need for now to use all-manual-nicely.sh
+
+#~ cd /home/ofbizDemo/trunk
+#~ svn up
+#~ ./gradlew --no-daemon pullAllPluginsSource
+#~ ./gradlew --no-daemon "ofbiz --shutdown"
+#~ ./gradlew --no-daemon cleanAll
+#~ ./gradlew --no-daemon loadAll
+#~ ./gradlew --no-daemon svnInfoFooter
+#~ ./gradlew --no-daemon ofbizBackground
