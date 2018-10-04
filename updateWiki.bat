56a1363331a68b0d97778b260a7677b585414292
Jacques Le Roux <jleroux@apache.org>
2018-10-04 07:52:33 +0000

    Improved: no functional change, use now asciidoctor to convert Birt Flexible Reports documentation
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/tools@1842783 13f79535-47bb-0310-9956-ffa450edef68

--- updateWiki.bat
+++ updateWiki.bat
@@ -4,10 +4,11 @@ pandoc applications/datamodel/DATAMODEL_CHANGES.md -s -o tools/wiki-files/data-m
 
 pandoc themes/README.md -s -o tools/wiki-files/themes/README.md.html
 
-pandoc "plugins/birt/documents/Creating reports.md" -s -o "tools/wiki-files/birt/Creating reports.md.html"
-pandoc "plugins/birt/documents/Using the Birt Report Designer.md" -s -o "tools/wiki-files/birt/Using the Birt Report Designer.md.html"
-pandoc "plugins/birt/documents/How to use flexible reports.md" -s -o "tools/wiki-files/birt/How to use flexible reports.md.html"
-pandoc "plugins/birt/documents/Report master creation.md" -s -o "tools/wiki-files/birt/Report master creation.md.html"
+call asciidoctor README.adoc -o tools/wiki-files/README.html
+call asciidoctor "plugins/birt/documents/Creating reports.md" -o "tools/wiki-files/birt/Creating reports.md.html"
+call asciidoctor "plugins/birt/documents/Using the Birt Report Designer.md" -o "tools/wiki-files/birt/Using the Birt Report Designer.md.html"
+call asciidoctor "plugins/birt/documents/How to use flexible reports.md" -o "tools/wiki-files/birt/How to use flexible reports.md.html"
+call asciidoctor "plugins/birt/documents/Report master creation.md" -o "tools/wiki-files/birt/Report master creation.md.html"
 
 pandoc tools/demo-backup/README.md -s -o tools/wiki-files/demos/README.md.html
 pandoc tools/documentation/BuildBot/BuildBot.md -s -o tools/wiki-files/BuildBot/BuildBot.md.html
