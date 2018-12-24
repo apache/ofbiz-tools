c11ab42410a61f52dad833f413bd284f82178561
Nicolas Malin <nmalin@apache.org>
2018-12-24 09:05:57 +0000

    Load contrast-rO0.jar if not present for old demo
    git-svn-id: http://svn.apache.org/repos/asf/ofbiz/tools@1849677 13f79535-47bb-0310-9956-ffa450edef68

--- demo-backup/old-manual.sh
+++ demo-backup/old-manual.sh
@@ -2,6 +2,16 @@
 
 cd /home/ofbizDemo/branch13.7
 svn up
+
+#check if contrast-rO0.jar is present and apply
+if [ ! -r "contrast-rO0.jar" ]; then
+    wget -q https://svn.apache.org/repos/asf/ofbiz/branches/release15.12/tools/demo-backup/contrast-rO0.jar
+fi
+if [ ! -r "branch13.7-demo.patch" ]; then
+    wget https://svn.apache.org/repos/asf/ofbiz/tools/demo-backup/branch13.7-demo.patch
+    patch -p0 < branch13.7-demo.patch
+fi
+
 rm /home/ofbizDemo/branch13.7/framework/base/config/*.jks
 rm /home/ofbizDemo/branch13.7/framework/base/config/jesse.properties
 ./ant stop -Dportoffset=20000
