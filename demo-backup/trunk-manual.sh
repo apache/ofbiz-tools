83e532c15cc46186ffe1be87bcf967291fb6a247
Jacques Le Roux <jacques.le.roux@les7arts.com>
2019-11-30 10:39:18 +0100

    Fixed: Update the demo
    (OFBIZ-11272)

--- demo-backup/trunk-manual.sh
+++ demo-backup/trunk-manual.sh
@@ -3,11 +3,11 @@
 echo This does not work, see OFBIZ-10287. You need for now to use all-manual-nicely.sh
 
 #~ cd /home/ofbizDemo/trunk
-#~ svn up
+#~ git pull
 #~ ./gradlew --no-daemon pullAllPluginsSource
 #~ ./gradlew --no-daemon "ofbiz --shutdown"
 #~ ./gradlew --no-daemon cleanAll
 #~ ./gradlew --no-daemon loadAll
-#~ ./gradlew --no-daemon svnInfoFooter
+#~ ./gradlew --no-daemon gitInfoFooter
 #~ ./gradlew --no-daemon ofbizBackground
 
--- demo-backup/trunk.sh
+++ demo-backup/trunk.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd /home/ofbizDemo/trunk
-svn up
+git pull
 rm /home/ofbizDemo/trunk/framework/base/config/*.jks
 
 ./gradlew --no-daemon pullAllPluginsSource
 ./gradlew --no-daemon terminateOfbiz
 ./gradlew --no-daemon cleanAll
 ./gradlew --no-daemon loadAll
-./gradlew --no-daemon svnInfoFooter
+./gradlew --no-daemon gitInfoFooter
 ./gradlew --no-daemon ofbizBackground
