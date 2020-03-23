832a5258bfe842ba837c3ad80c12c31837ddab0a
Swapnil M Mane <swapnil.mane@hotwaxsystems.com>
2020-03-23 07:56:40 +0530

    Improved: Added the echo information related to OFBIZ-10287 (OFBIZ-11472)

--- demo-backup/stable-manual.sh
+++ demo-backup/stable-manual.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo This cant be used alone, see OFBIZ-10287. You need for now to use all-manual-nicely.sh
 cd /home/ofbizDemo/branch17.12
 git pull
 rm /home/ofbizDemo/branch17.12/framework/base/config/*.jks
