e56d2032f03513dacc31fd0aed9c8a9afb7a38d2
Jacques Le Roux <jacques.le.roux@les7arts.com>
2020-09-09 19:42:18 +0200

    Improved: now using AsciiDoc in source
    We no longer need to generate:
    wiki-files/data-model/DATAMODEL_CHANGES.md.html
    wiki-files/themes/README.md.html

--- updateWiki.bat
+++ updateWiki.bat
@@ -2,10 +2,6 @@ cd tools
 
 call asciidoctor ..\README.adoc -o wiki-files/README.html
 
-pandoc ..\applications/datamodel/DATAMODEL_CHANGES.md -s -o wiki-files/data-model/DATAMODEL_CHANGES.md.html
-
-pandoc ..\themes/README.md -s -o wiki-files/themes/README.md.html
-
 pandoc demo-backup/README.md -s -o wiki-files/demos/README.md.html
 pandoc documentation/BuildBot/BuildBot.md -s -o wiki-files/BuildBot/BuildBot.md.html
 
