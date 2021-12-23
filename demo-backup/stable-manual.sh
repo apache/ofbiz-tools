8e4c13f0ade24c0da0a8bdaacaa8eaaa05556d44
Jacques Le Roux <jacques.le.roux@les7arts.com>
2021-12-23 09:23:25 +0100

    Fixed: few issues with patches in demo
    Oops, I used // instead of # as comments in bash :/

--- demo-backup/stable-manual.sh
+++ demo-backup/stable-manual.sh
@@ -4,19 +4,19 @@ echo This cant be used alone, see OFBIZ-10287. You need for now to use all-manua
 
 cd /home/ofbizDemo/branch18.12
 
-// checkout patched files before patching them, else git pull would not work
+# checkout patched files before patching them, else git pull would not work
 git checkout framework/webapp/config/url.properties
 git checkout framework/webapp/config/fop.xconf
 git pull
 patch -p0 < /home/ofbizDemo/branch18.12/url.properties.patch
 patch -p0 < /home/ofbizDemo/branch18.12/fop.xconf.patch
 
-// We don't want *.jks, we use "Let's encrypt"
+# We don't want *.jks, we use "Let's encrypt"
 rm /home/ofbizDemo/branch18.12/framework/base/config/*.jks
 
 
 ./gradlew --no-daemon pullAllPluginsSource
-// Here no need to check out, it's not a repo. So patching is OK
+# Here no need to check out, it's not a repo. So patching is OK
 cd /home/ofbizDemo/branch18.12/plugins
 patch -p0 < /home/ofbizDemo/branch18.12/solr.config.patch
 cd ..
--- demo-backup/trunk.sh
+++ demo-backup/trunk.sh
@@ -3,19 +3,19 @@
 echo This cant be used alone, see OFBIZ-10287. You need for now to use all-manual-nicely.sh
 
 cd /home/ofbizDemo/trunk
-// checkout patched files before patching them, else git pull would not work
+# checkout patched files before patching them, else git pull would not work
 git checkout framework/webapp/config/url.properties
 git checkout framework/webapp/config/fop.xconf
 git pull
 patch -p0 < /home/ofbizDemo/trunk/url.properties.patch
 patch -p0 < /home/ofbizDemo/trunk/fop.xconf.patch
 
-// We don't want *.jks, we use "Let's encrypt"
+# We don't want *.jks, we use "Let's encrypt"
 rm /home/ofbizDemo/trunk/framework/base/config/*.jks
 
 
 ./gradlew --no-daemon pullAllPluginsSource
-// Here no need to check out, it's not a repo. So patching is OK
+# Here no need to check out, it's not a repo. So patching is OK
 cd /home/ofbizDemo/trunk/plugins
 patch -p0 < /home/ofbizDemo/trunk/solr.config.patch
 cd ..
