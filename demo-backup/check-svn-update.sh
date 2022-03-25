23eefb8f475ba8c125013282f6450bf1d4311d94
Jacques Le Roux <jacques.le.roux@les7arts.com>
2022-03-25 17:37:34 +0100

    10 mins is too short for testing, 20 mins is OK Updates fop.xconf.patch and solr.config.patch in next

--- demo-backup/check-svn-update.sh
+++ demo-backup/check-svn-update.sh
@@ -4,7 +4,7 @@
 # for ofbizDemo user
 # sudo crontab -e
 # 0 3 * * * nice -n 19 ionice -c2 -n7 /home/ofbizDemo/check-svn-update.sh > /home/ofbizDemo/cronlog-svn-update.log 2>&1
-# to test:  */10 * * * *
+# to test:  */20 * * * *
 
 cd /home/ofbizDemo/
 
