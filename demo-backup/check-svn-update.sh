db922eea067a90d7ac2511aca61fbd3b40896bc4
Jacques Le Roux <jacques.le.roux@les7arts.com>
2022-03-25 13:29:33 +0100

    Clearer, simpler comments

--- demo-backup/check-svn-update.sh
+++ demo-backup/check-svn-update.sh
@@ -3,8 +3,8 @@
 # Restart OFBiz demos  each morning at 3
 # for ofbizDemo user
 # sudo crontab -e
-# add : 0 3 * * * nice -n 19 ionice -c2 -n7 /home/ofbizDemo/check-svn-update.sh > /home/ofbizDemo/cronlog-svn-update.log 2>&1
-# to test:  */10 * * * * ....
+# 0 3 * * * nice -n 19 ionice -c2 -n7 /home/ofbizDemo/check-svn-update.sh > /home/ofbizDemo/cronlog-svn-update.log 2>&1
+# to test:  */10 * * * *
 
 cd /home/ofbizDemo/
 
