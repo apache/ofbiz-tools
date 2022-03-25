b78f382d14a0eefe4c16fba95c1d6d1b3b950bfd
Jacques Le Roux <jacques.le.roux@les7arts.com>
2022-03-25 08:31:44 +0100

    Forces all path
    ./all-manual.sh was working before in check-svn-update.sh, but no longer.

--- demo-backup/check-svn-update.sh
+++ demo-backup/check-svn-update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Restart each morning at 3
+# Restart OFBiz demos  each morning at 3
 # for ofbizDemo user
-# crontab -e
+# sudo crontab -e
 # add : 0 3 * * * nice -n 19 ionice -c2 -n7 /home/ofbizDemo/check-svn-update.sh > /home/ofbizDemo/cronlog-svn-update.log 2>&1
 # to test:  */10 * * * * ....
 
-./all-manual.sh
+/home/ofbizDemo/all-manual.sh
